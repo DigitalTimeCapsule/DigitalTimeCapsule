b9cfa46b31a5f6f8e4df908ef80ae0d956d86624
Eshar Heer <ec241019@qmul.ac.uk>
2025-03-05 11:51:17 +0000

    Initial Project Setup

---
+++ backend/digitaltimecapsule/src/main/java/com/digitaltimecapsule/digitaltimecapsule/DigitaltimecapsuleApplication.java
@@ -0,0 +1,11 @@
+package com.digitaltimecapsule.digitaltimecapsule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitaltimecapsuleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DigitaltimecapsuleApplication.class, args);
+    }
+}
---
+++ backend/digitaltimecapsule/src/test/java/com/digitaltimecapsule/digitaltimecapsule/DigitaltimecapsuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitaltimecapsule.digitaltimecapsule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitaltimecapsuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
