a2ce311dc7022009b017fb38afdc957bdacde736
Eshar Heer <ec241019@qmul.ac.uk>
2025-04-03 16:17:51 +0100

    removed lombok and added getters and setters manually

--- backend/digitaltimecapsule/src/main/java/com/digitaltimecapsule/digitaltimecapsule/model/User.java
+++ backend/digitaltimecapsule/src/main/java/com/digitaltimecapsule/digitaltimecapsule/model/User.java
@@ -6,17 +6,9 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Entity
 @Table(name = "users")
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -27,4 +19,37 @@ public class User {
 
     @Column(nullable = false)
     private String password;
+
+    public User() {
+
+    }
+
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
